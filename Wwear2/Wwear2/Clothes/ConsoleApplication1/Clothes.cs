fa13b858fa586b32f0931d1a1ff3da231999a229
ViralPanda <patel.2004@osu.edu>
2014-10-04 05:57:45 -0400

    Clothes Class / Console Testing
    Clothes Class with Enumerated Types (need moved) and console to test out
    clothes class

---
+++ Wwear2/Wwear2/Clothes/ConsoleApplication1/Clothes.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Clothes
+    {
+        public enum types { notype, hat, jacket, sweater, tanktop, tShirt, jeans, shorts, pants, sweatPants, coat, shoes, sandels, flipflops };
+        public enum tags { notag, thick, light, longSleeved, shortSleeved, noSleeve, heavy, fullLength, halfLength, threeForthsLength, closedToe, openToe, waterProof, insulated };
+
+        private types typeOfClothes;
+        private tags[] clothesTags = new tags[5];
+        private int numberOfTags = 0;
+        private string p;
+
+        public void setType(types typeOfCloth){
+            typeOfClothes = typeOfCloth;
+        }
+        public void addTag(string sTag)
+        {
+            tags tag;
+            switch (sTag)
+            {
+                case "thick":
+                    tag = tags.thick;
+                    break;
+                case "light":
+                    tag = tags.light;
+                    break;
+                case "long sleave":
+                    tag = tags.longSleeved;
+                    break;
+                case "short sleave":
+                    tag = tags.shortSleeved;
+                    break;
+                case "no sleave":
+                    tag = tags.noSleeve;
+                    break;
+                case "heavy":
+                    tag = tags.heavy;
+                    break;
+                case "full length":
+                    tag = tags.fullLength;
+                    break;
+                case "half length":
+                    tag = tags.halfLength;
+                    break;
+                case "three forths length":
+                    tag = tags.threeForthsLength;
+                    break;
+                case "closed toe":
+                    tag = tags.closedToe;
+                    break;
+                case "open toe":
+                    tag = tags.openToe;
+                    break;
+                case "waterproof":
+                    tag = tags.waterProof;
+                    break;
+                case "insulated":
+                    tag = tags.insulated;
+                    break;
+                default:
+                    tag = tags.notag;
+                    Console.WriteLine("No such tag found!");
+                    Environment.Exit(0);
+                    break;
+            }
+            if (numberOfTags < 5)
+            {
+
+                clothesTags[numberOfTags] = tag;
+                numberOfTags++;
+            }
+        }
+        public void removeTag(string sTag)
+        {
+            tags tag;
+            switch (sTag)
+            {
+                case "thick":
+                    tag = tags.thick;
+                    break;
+                case "light":
+                    tag = tags.light;
+                    break;
+                case "long sleave":
+                    tag = tags.longSleeved;
+                    break;
+                case "short sleave":
+                    tag = tags.shortSleeved;
+                    break;
+                case "no sleave":
+                    tag = tags.noSleeve;
+                    break;
+                case "heavy":
+                    tag = tags.heavy;
+                    break;
+                case "full length":
+                    tag = tags.fullLength;
+                    break;
+                case "half length":
+                    tag = tags.halfLength;
+                    break;
+                case "three forths length":
+                    tag = tags.threeForthsLength;
+                    break;
+                case "closed toe":
+                    tag = tags.closedToe;
+                    break;
+                case "open toe":
+                    tag = tags.openToe;
+                    break;
+                case "waterproof":
+                    tag = tags.waterProof;
+                    break;
+                case "insulated":
+                    tag = tags.insulated;
+                    break;
+                default:
+                    tag = tags.notag;
+                    Console.WriteLine("No such tag found!");
+                    Environment.Exit(0);
+                    break;
+            }
+            for (int i = 0; i < numberOfTags; i++)
+            {
+                if (clothesTags[i] == tag)
+                {
+                    clothesTags[i] = tags.notag;
+                }
+                if (clothesTags[i] == tags.notag)
+                {
+                    clothesTags[i] = clothesTags[i + 1];
+                    clothesTags[i + 1] = tags.notag;
+                }
+            }
+            numberOfTags--;
+        }
+
+        public Clothes(types typeOfCloth)
+        {
+            this.typeOfClothes = typeOfCloth;
+        }
+
+        public Clothes(string p)
+        {
+            // TODO: Complete member initialization
+            switch (p)
+            {
+                case "hat":
+                    this.typeOfClothes = types.hat;
+                    break;
+                case "jacket":
+                    typeOfClothes = types.jacket;
+                    break;
+                case "sweater":
+                    typeOfClothes = types.sweater;
+                    break;
+                case "tanktop":
+                    typeOfClothes = types.tanktop;
+                    break;
+                case "t-shirts":
+                    typeOfClothes = types.tShirt;
+                    break;
+                case "jeans":
+                    typeOfClothes = types.jeans;
+                    break;
+                case "shorts":
+                    typeOfClothes = types.shorts;
+                    break;
+                case "pants":
+                    typeOfClothes = types.pants;
+                    break;
+                case "sweat pants":
+                    typeOfClothes = types.sweatPants;
+                    break;
+                case "coat":
+                    typeOfClothes = types.coat;
+                    break;
+                case"shoes":
+                    typeOfClothes = types.shoes;
+                    break;
+                case "sandles":
+                    typeOfClothes = types.sandels;
+                    break;
+                case "flip flops":
+                    typeOfClothes = types.flipflops;
+                    break;
+                default:
+                    typeOfClothes = types.notype;
+                    Console.WriteLine("No such type found!\nThe application will now terminate!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        public static void printTags(Clothes a)
+        {
+            for (int i = 0; i < a.numberOfTags; i++)
+            {
+                Console.WriteLine(a.clothesTags[i]);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter type of cloth: ");
+            Clothes cloth1 = new Clothes(Console.ReadLine());
+            Console.Write("Enter tag: ");
+            string tag = tag = Console.ReadLine();
+            int count = cloth1.numberOfTags;
+            do
+            {
+                Console.Write("Enter tag: ");
+                cloth1.addTag(tag);
+                tag = Console.ReadLine();
+            } while (!String.IsNullOrEmpty(tag) && count <= 5);
+            Console.Write("Enter a tag to remove: ");
+            cloth1.removeTag(Console.ReadLine());
+            Console.WriteLine("Number of Tags: " + cloth1.numberOfTags);
+            printTags(cloth1);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Wwear2/Wwear2/Clothes/ConsoleApplication1/Program.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Jacket
+    {
+        public static bool heavy;
+        public static bool light;
+        public static bool windBreaker;
+        public static int cold, freeze, breeze, hot, humid, rainy, windy, snow;
+        public Jacket()
+        {
+        }
+        public static void isHeavy(bool a)
+        {
+            heavy = a;
+            if (a)
+            {
+                light = false;
+                windBreaker = false;
+            }
+        }
+        public static void isLight(bool a)
+        {
+            light = a;
+            if (a)
+            {
+                heavy = false;
+                windBreaker = false;
+            }
+        }
+        public static void isWindBreaker(bool a)
+        {
+            windBreaker = a;
+            if (a)
+            {
+                light = false;
+                heavy = false;
+            }
+        }
+        public static void setValues()
+        {
+            if (heavy)
+            {
+                cold = 8;
+                freeze = 10;
+                breeze = 0;
+                hot = 0;
+                humid = 0;
+                rainy = 5;
+                windy = 0;
+                snow = 10;
+            }
+            else if (light)
+            {
+                cold = 10;
+                freeze = 7;
+                breeze = 8;
+                hot = 2;
+                humid = 0;
+                rainy = 10;
+                windy = 7;
+                snow = 2;
+            }
+            else
+            {
+                cold = 2;
+                freeze = 0;
+                breeze = 10;
+                hot = 5;
+                humid = 5;
+                rainy = 6;
+                windy = 10;
+                snow = 0;
+            }
+        }
+    }
+    class Sweater
+    {
+        public static int cold = 10, freeze = 6, breeze = 4, hot = 0, humid = 0, rainy = 0, windy = 6, snow = 0;
+        public Sweater()
+        {
+        }
+    }
+    class RegularTop
+    {
+        public static bool longSleeve = false, shortSleeve = false, tankTop = false;
+        public static int cold, freeze, breeze, hot, humid, rainy, windy, snow;
+        public RegularTop()
+        {
+        }
+        public static void isLong(bool a)
+        {
+            longSleeve = a;
+        }
+        public static void isShort(bool a)
+        {
+            shortSleeve = a;
+        }
+        public static void isTankTop(bool a)
+        {
+            tankTop = a;
+        }
+        public static void setValues()
+        {
+            if (longSleeve)
+            {
+                cold = 8;
+                freeze = 10;
+                breeze = 6;
+                hot = 0;
+                humid = 0;
+                rainy = 5;
+                windy = 10;
+                snow = 10;
+            }
+            else if (shortSleeve)
+            {
+                cold = 7;
+                freeze = 8;
+                breeze = 7;
+                hot = 9;
+                humid = 8;
+                rainy = 5;
+                windy = 0;
+                snow = 4;
+            }
+            else
+            {
+                cold = 0;
+                freeze = 0;
+                breeze = 5;
+                hot = 10;
+                humid = 10;
+                rainy = 5;
+                windy = 0;
+                snow = 0;
+            }
+        }
+
+        class Program
+        {
+            const int REFERENCETEMP = 72;
+            static bool setCold(int temp, int wc)
+            {
+                bool cold;
+                int feltTemp = Convert.ToInt32(0.3 * wc + 0.7 * temp);
+                if ((REFERENCETEMP - feltTemp > 10 && REFERENCETEMP - feltTemp < 25) && feltTemp > 40)
+                {
+                    cold = true;
+                }
+                else
+                {
+                    cold = false;
+                }
+                return cold;
+            }
+            static bool setFreeze(int temp, int wc)
+            {
+                bool freeze = false;
+                int feltTemp = Convert.ToInt32(0.3 * wc + 0.7 * temp);
+                if (temp < 40 || REFERENCETEMP - feltTemp > 25)
+                {
+                    freeze = true;
+                }
+                return freeze;
+            }
+            static bool setHot(int temp, int wc)
+            {
+                bool hot = false;
+                int feltTemp = Convert.ToInt32(0.3 * wc + 0.7 * temp);
+                if (temp > 80 || feltTemp - REFERENCETEMP > 5)
+                {
+                    hot = true;
+                }
+                return hot;
+            }
+            static bool setRainy(int chanceRain)
+            {
+                bool rainChance = false;
+                if (chanceRain > 50)
+                {
+                    rainChance = true;
+                }
+                return rainChance;
+            }
+            static void setWind(ref bool nowind, ref bool breeze, ref bool wind, int windSpeed)
+            {
+                nowind = false;
+                breeze = false;
+                wind = false;
+                if (windSpeed < 2)
+                {
+                    nowind = true;
+                }
+                else if (windSpeed < 10)
+                {
+                    breeze = true;
+                }
+                else
+                {
+                    wind = true;
+                }
+            }
+            static bool setHumid(int humidity)
+            {
+                bool humid = false;
+                if (humidity > 30)
+                {
+                    humid = true;
+                }
+                return humid;
+            }
+
+            /* static void Main(string[] args)
+             {
+                 int temp = 70, windChill = 65, windSpeed = 10, humidity = 20, chancePres = 40;
+                 bool cold = false, freeze = false, breeze = false, hot = false, humid = false, rainy = false, windy = false, snow, normal = false, noWind = false;
+                 cold = setCold(temp, windChill);
+                 if (cold)
+                 {
+                     rainy = setRainy(chancePres);
+                 }
+                 else
+                 {
+                     freeze = setFreeze(temp, windChill);
+                     if (freeze)
+                     {
+                         snow = setRainy(chancePres);
+                     }
+                     else
+                     {
+                         hot = setHot(temp, windChill);
+                         if (hot)
+                         {
+                             rainy = setRainy(chancePres);
+                         }
+                         else
+                         {
+                             normal = true;
+                             rainy = setRainy(chancePres);
+                         }
+                     }
+                 }
+                 setWind(ref noWind,ref breeze,ref windy, windSpeed);
+                 humid = setHumid(humidity); 
+             }
+         }
+             */
+        }
+    }
+}
